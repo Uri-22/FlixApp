cf628ae71e4bdc0577a9bae8dd71d6d15f94d890
Uriel Sejas <urielsejas@gmail.com>
2021-02-19 20:08:41 -0500

    updated table view with title and table view cell

---
+++ FlixApp/MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCell.swift
+//  FlixApp
+//
+//  Created by Uriel Sejas  on 2/19/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
